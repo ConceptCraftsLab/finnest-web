c393b97cd0a368d3fd3fe3f719c7eb6f45266907
dhruv <dhruv2295@gmail.com>
2023-12-05 00:14:51 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Replace this with your target date
+const targetDate = new Date('2023-12-31T00:00:00Z');
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeLeft = targetDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerText = formatTime(days);
+  document.getElementById('hours').innerText = formatTime(hours);
+  document.getElementById('minutes').innerText = formatTime(minutes);
+  document.getElementById('seconds').innerText = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? '0' + time : time;
+}
+
+// Initial call to set countdown on page load
+updateCountdown();
+
+// Update countdown every second
+setInterval(updateCountdown, 1000);
